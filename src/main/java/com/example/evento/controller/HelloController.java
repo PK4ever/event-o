78f8e701f9e2a637f959cc90f8f30793b9126b58
Paul <paulkobe.pk@gmail.com>
2018-04-05 18:09:31 -0400

    added view

--- src/main/java/com/example/evento/controller/HelloController.java
+++ src/main/java/com/example/evento/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.example.evento.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -16,6 +22,12 @@ public class HelloController {
     @ResponseBody
     String home() {
         return " the begining of event-o ";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 
 }
