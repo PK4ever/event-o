6a3e6dc84c55efbc0058e665da162aa862074522
csaintil <cashleysaintilus@gmail.com>
2018-04-16 17:51:54 -0400

    search fully fuctional

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -3,8 +3,14 @@ $(document).ready(function() {
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(success,fail);
     }
+    // $("#searchSubmit").click(getEvents(location,""));
+    $("#searchSubmit").click(function() {
 
+        getEvents($("#location").val(),$("#keyword").val());
+
+    });
 });
+
 
 function success(position) {
     $.getJSON(
@@ -20,43 +26,47 @@ function success(position) {
             // alert(json.name+ " , "+ json.sys.country);
             $("#location").html(location);
             $("#weatherDescription").html(json.weather[0].description);
-            getEvents();
+
         }
     );
     $.getJSON(
         "https://maps.googleapis.com/maps/api/geocode/json?" +
         "latlng=+"+position.coords.latitude+","+position.coords.longitude+"&key=AIzaSyDAdYEKAXvFad5gMEjpY12XLYPACrNuDt0",
         function (data) {
+             console.log(data);
 
             var location = data.results[0].address_components[3].long_name+", "+data.results[0].address_components[5].short_name;
-            getEvents(location);
+            getEvents(location,"" );
         });
 }
 function fail() {
-    getEvents("Brooklyn NY");
+    getEvents("Brooklyn NY", "");
 }
 
-function getEvents(location) {
+function getEvents(location, keyword) {
 
     var oArgs = {
         app_key: "qX2dNH9TZGLvBT8B",
-        // q: "music",
+         q: keyword,
         where: location,
         // where: "syracuse",
         page_size: 10,
         sort_order: "popularity",
     }
-    EVDB.API.call("/events/search", oArgs, function(oData) {
+    EVDB.API.call("/events/search",
+        oArgs,
+        function(oData) {
         // alert(oData.events.event.length);
         for(var x = 0; x < oData.events.event.length; x++){
             if(oData.events.event[x].image) {
-                $("#title").append("<li><p>" + oData.events.event[x].title + "</p>" +
+                $("#title").innerHTML("<li><p>" + oData.events.event[x].title + "</p>" +
                     "<p>" + oData.events.event[x].venue_address + "</p>" +
                     "<img src='" + oData.events.event[x].image.medium.url + "'></li>");
             }else{
-                $("#title").append("<li><p>" + oData.events.event[x].title + "</p>" +
+                $("#title").innerHTML("<li><p>" + oData.events.event[x].title + "</p>" +
                     "<p>" + oData.events.event[x].venue_address + "</p>" +
                     "</li>");
+
             }
         }
         // $.getJSON(
@@ -66,6 +76,7 @@ function getEvents(location) {
         //     }
         // );
         // Note: this relies on the custom toString() methods below
-    });
 
+    });
 }
+
